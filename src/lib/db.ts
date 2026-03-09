5226ad525e7de1e8042ce20d0652edacc5c95527
Ferdinand-MZ <ferdinandmaulanazafauzi@gmail.com>
2026-03-09 10:07:52 +0700

    set up

---
+++ src/lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from '@generated/prisma';
+
+// PrismaClient is expensive to create, so we create a single instance and reuse it across the app.
+// This is especially important in development, where hot reloading can cause multiple instances to be created.
+let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
