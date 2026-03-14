7ac561923e84d478d45a09e6c929ddfc9ef43bdb
Ferdinand-MZ <ferdinandmaulanazafauzi@gmail.com>
2026-03-14 12:25:59 +0700

    06: background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,37 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import { requireAuth } from "@/lib/auth-utils";
-import { caller } from "@/trpc/server";
 import { LogoutButton } from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
+import {toast} from "sonner";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
 
-  const data = await caller.getUsers();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job Queued");
+      },
+    })
+  );
 
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
-      <div>
-      {JSON.stringify(data, null, 2)}
-      </div>
+      <div>{JSON.stringify(data, null, 2)}</div>
 
-     <LogoutButton />
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>
+        Create Workflow
+      </Button>
+
+      <LogoutButton />
     </div>
-  )
-}
+  );
+};
 
 export default Page;
