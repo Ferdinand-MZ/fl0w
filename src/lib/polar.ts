b6f52808260449f966d5066ecd104c7818d2c46e
Ferdinand-MZ <ferdinandmaulanazafauzi@gmail.com>
2026-03-20 18:56:09 +0700

    13: Workflows UI

--- src/lib/polar.ts
+++ src/lib/polar.ts
@@ -5,6 +5,5 @@ console.log("TOKEN:", process.env.POLAR_ACCESS_TOKEN);
 export const polarClient = new Polar({
     accessToken: process.env.POLAR_ACCESS_TOKEN,
     // server: process.env.NODE_ENV !== "production" ? "sandbox" : "production", // TO DO : change in production
-    server: "production", // 🔥 paksa production
-    // server: "sandbox", // TO DO : change in production
+    server: "sandbox", // 🔥 paksa production
 });
