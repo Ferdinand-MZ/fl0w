23b70a89fa8a8bad84fedb40ea82272bc0442d13
Ferdinand-MZ <ferdinandmaulanazafauzi@gmail.com>
2026-03-22 09:54:30 +0700

    17: Editor State

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
+import {Provider} from "jotai";
 import {NuqsAdapter} from "nuqs/adapters/next/app";
 
 const geistSans = Geist({
@@ -32,8 +33,10 @@ export default function RootLayout({
       >
         <TRPCReactProvider>
           <NuqsAdapter>
-          {children}
-          <Toaster />
+            <Provider>
+              {children}
+              <Toaster />
+            </Provider>
           </NuqsAdapter>
         </TRPCReactProvider>
       </body>
--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -21,8 +21,9 @@ import {
 } from "@xyflow/react";
 import { nodeComponents } from "@/config/node-components";
 import { AddNodeButton } from "./add-node-buttons";
-
 import '@xyflow/react/dist/style.css';  
+import { useSetAtom } from "jotai";
+import { editorAtom } from "../store/atoms";
 
 export const EditorLoading = () => {
     return <LoadingView message="Loading editor..."/>
@@ -34,6 +35,8 @@ export const EditorError = () => {
 
 export const Editor = ({workflowId}: {workflowId: string}) => {
     const {data: workflow} = useSuspenseWorkflow(workflowId);
+
+    const setEditor = useSetAtom(editorAtom);
 
     const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
     const [edges, setEdges] = useState<Edge[]>(workflow.edges);
@@ -60,7 +63,13 @@ export const Editor = ({workflowId}: {workflowId: string}) => {
                 onEdgesChange={onEdgesChange}
                 onConnect={onConnect}
                 nodeTypes={nodeComponents}
+                onInit={setEditor} 
                 fitView
+                snapGrid={[10, 10]}
+                snapToGrid
+                panOnScroll
+                panOnDrag={false}
+                selectionOnDrag
                 proOptions={{
                     hideAttribution: true,
                 }}
